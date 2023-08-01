b38fcff0f347a772864d9e3e68ddd3f9dc568406
Nurzhan Kongratbayev <kongratbayevn@gmail.com>
2023-08-01 12:17:41 +0600

    auth -service fix

--- src/domains/auth/schemas/verify-or-change-pwd.schema.ts
+++ src/domains/auth/schemas/verify-or-change-pwd.schema.ts
@@ -1,7 +1,7 @@
 import z from 'zod';
 
 export const VerifyOrChangePwdSchema = z.object({
-  body: z.object({ email: z.string(), password: z.string() }).strict(),
+  body: z.object({ email: z.string().optional(), password: z.string() }).strict(),
   query: z.object({}).strict(),
   params: z.object({linkId: z.string()}).strict(),
 });
