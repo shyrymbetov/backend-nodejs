8055c5b20abe763d64da687fb1d5e1713e58a8da
Nurzhan Kongratbayev <kongratbayevn@gmail.com>
2023-08-03 16:48:54 +0600

    uuid validate fix

--- src/shared/util.service.ts
+++ src/shared/util.service.ts
@@ -1,5 +1,8 @@
 
 
-export function isValidUUID(uuid: string): boolean {
+export function isValidUUID(uuid: string | undefined): boolean {
+  if (!uuid) {
+    return false;
+  }
   return /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[4][0-9a-fA-F]{3}-[89AB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/i.test(uuid);
 }
