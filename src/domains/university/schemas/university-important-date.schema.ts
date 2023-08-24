5ddb5737f9ab5a9e9f291ce26591bd723b1ff3a1
KONGRATBAYEV NURZHAN <kongratbayevn@gmail.com>
2023-08-24 17:45:17 +0600

    university schema nullable

--- src/domains/university/schemas/university-important-date.schema.ts
+++ src/domains/university/schemas/university-important-date.schema.ts
@@ -3,12 +3,12 @@ import {isValidUUID} from "../../../shared/util.service";
 
 export const UniversityImportantDateSchema = z.object({
     id: z.string().optional().transform((val) => isValidUUID(val) ? val: undefined),
-    name: z.string().optional(),
+    name: z.string().optional().nullable().transform(val => val ?? undefined),
     deadline: z.string().datetime().optional().transform(val =>val ? new Date(Date.parse(val)) : undefined),
     additionalDates: z.array(
         z.object({
-            date: z.string().datetime().optional().transform(val => val ? new Date(Date.parse(val)) : undefined),
-            description: z.string().optional()
+            date: z.string().datetime().nullable().optional().transform(val => val ? new Date(Date.parse(val)) : undefined),
+            description: z.string().optional().nullable().transform(val => val ?? undefined)
         })
     ).optional().transform((val) => val ?? []),
 });
