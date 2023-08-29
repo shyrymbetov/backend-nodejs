0911e38c96101cf0501e3b55d799bbdc63998553
KONGRATBAYEV NURZHAN <kongratbayevn@gmail.com>
2023-08-29 15:20:25 +0600

    docker-compose fix

--- src/seeds/1692781153896-add-admin-user.ts
+++ src/seeds/1692781153896-add-admin-user.ts
@@ -10,8 +10,8 @@ export class AddAdminUser1692781153896 implements MigrationInterface {
 
         password = crypto.pbkdf2Sync(password, config.pwd.salt, 100, 64, 'sha256').toString(`hex`)
 
-        await queryRunner.query(`insert into user_entity ("email", "first_name", "last_name", "hashed_password", "birth_date", "phone" , "role", "region_id", "local_id")
-            values('admin@gmail.com', 'Nurzhan', 'Kermenbayev', '${password}', '2018-01-01', '87059038943', 'admin', '0a492740-18b7-4b45-9b74-c0879dcfeedd', '0a492740-18b7-4b45-9b74-c0879dcfeedd' )`);
+        await queryRunner.query(`insert into user_entity ("email", "first_name", "last_name", "hashed_password", "birth_date", "phone" , "role", "region_id", "local_id", "active", "verified")
+            values('admin@gmail.com', 'Nurzhan', 'Kermenbayev', '${password}', '2018-01-01', '87059038943', 'admin', '0a492740-18b7-4b45-9b74-c0879dcfeedd', '0a492740-18b7-4b45-9b74-c0879dcfeedd', true, true )`);
 
     }
 
